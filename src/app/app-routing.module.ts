aa9f37524dfa18c4dcf03e04c9c22c1c36de09b5
Victor Torres <victormiguel.torres.next@bbva.com>
2019-03-19 23:11:56 -0600

    added home component, global CSS and Post and User models

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'welcome', 
+  {
+    path: 'welcome',
     component: WelcomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HomeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
