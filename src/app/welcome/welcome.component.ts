7699b6844e541966468e8ac54fca2348ea965d50
Victor Torres <victormiguel.torres.next@bbva.com>
2019-03-22 16:54:57 -0600

    Downgrade angular version, added image for welcome.html

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -2,31 +2,56 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as M from '../../assets/materialize/js/materialize.min.js';
 
+
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
+
+
+
+
 export class WelcomeComponent implements OnInit {
 
-  title = 'tablero virtual NEXT';
-  options = {
-    fullWidth : true
+  title = 'Quick board';
+  optionsCarousel = {
+
   }
 
   constructor(
-    private _router: Router
+    private _router: Router,
   ) { }
 
   ngOnInit() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, this.options);
+    console.log('ngOnInit')
+    //var elems = document.querySelectorAll('.carousel');
+    //var instances = M.Carousel.init(elems, this.optionsCarousel);
+
   }
 
   redirectHome() {
     this._router.navigate(['./home']);
   }
 
-  
+  onSignIn(googleUser) {
+    console.log('hi');
+  }
 
+  /*socialSignIn(socialPlatform : string) {
+    let socialPlatformProvider;
+    socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+
+    this.socialAuthService.signIn(socialPlatformProvider).then(
+      (userData) => {
+        console.log(socialPlatform+" sign in data : " , userData);
+        // Now sign-in with userData
+        // ...
+            
+      }
+    );
+  }*/
+  
 }
+
